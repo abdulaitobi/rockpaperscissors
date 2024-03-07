422067e6c00da0bd3f777e72fe043dc573c71f9a
abdulaitobi <abdulaitobi14@gmail.com>
2024-03-06 18:12:50 -0800

    Started js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const computerChoiceDisplay = document.getElementById('computerChoiceDisplay');
+const playerChoiceDisplay = document.getElementById('playerChoiceDisplay');
+var playercount = 0;
+var compcount = 0;
+var score = 0;
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
@@ -15,47 +23,51 @@ function getComputerChoice() {
     return val;
 }
 
+rock.addEventListener('click', playRound('rock', getComputerChoice()));
+paper.addEventListener('click', playRound('paper', getComputerChoice()));
+scissors.addEventListener('click', playRound('scissors', getComputerChoice()));
+
 function playRound(playerChoice, computerSelection){
     console.log(playerChoice);
     console.log(computerSelection);
+
+    const playerVal = document.createElement('p');
+    const compVal = document.createElement('p');
+    playerChoiceDisplay.appendChild(playerVal);
+    playerVal.textContent = playerChoice;
+    computerChoiceDisplay.appendChild(compVal);
+    compVal.textContent = computerSelection;
+
     if((playerChoice.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") || (playerChoice.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") || (playerChoice.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock"))
     {
-        return -1;
+        score = -1;
     }
     if((playerChoice.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") || (playerChoice.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") || (playerChoice.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"))
     {
-        return 1;
+        score = 1;
     }
     if(playerChoice.toLowerCase() === computerSelection.toLowerCase()){
-        console.log("Its a tie, play again");
-        return (playRound(prompt("Type Rock, Paper, or Scissors"), getComputerChoice()));
-    }
-}
 
-function game(){
-    var playercount = 0;
-    var compcount = 0;
-    for (let index = 0; index < 5; index++) {
-        player = prompt("Type Rock, Paper, or Scissors");
-        computer = getComputerChoice();
-        var result = playRound(player, computer);
-        if(result == 1)
+    }
+
+    while (!(playercount || compcount) == 5) {
+        if(score == 1)
         {
             playercount++;
         }
-        else if(result == -1)
+        else if(score == -1)
         {
             compcount++;
         }
     }
     if(playercount > compcount)
     {
-        console.log("You win " + playercount + " - " + compcount);
+        alert("You win " + playercount + " - " + compcount);
     }
     else if(compcount > playercount)
     {
-        console.log("You lose " + compcount + " - " + playercount);
+        alert("You lose " + compcount + " - " + playercount);
     }
-    
 }
+
 

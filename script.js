d8f6954e1b54c2c7f97b4d1b76283b1e82a245f4
abdulaitobi <abdulaitobi14@gmail.com>
2024-03-11 23:28:54 -0700

    Finished product

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const computerChoiceDisplay = document.getElementById('computerChoiceDisplay');
 const playerChoiceDisplay = document.getElementById('playerChoiceDisplay');
+const scores = document.getElementById('scores');
+const playerScore = document.getElementById('playerScore');
+const compScore = document.getElementById('compScore');
+const scoreMessage = document.createElement('p');
 var playercount = 0;
 var compcount = 0;
 var score = 0;
@@ -23,14 +27,7 @@ function getComputerChoice() {
     return val;
 }
 
-rock.addEventListener('click', playRound('rock', getComputerChoice()));
-paper.addEventListener('click', playRound('paper', getComputerChoice()));
-scissors.addEventListener('click', playRound('scissors', getComputerChoice()));
-
 function playRound(playerChoice, computerSelection){
-    console.log(playerChoice);
-    console.log(computerSelection);
-
     const playerVal = document.createElement('p');
     const compVal = document.createElement('p');
     playerChoiceDisplay.appendChild(playerVal);
@@ -38,35 +35,63 @@ function playRound(playerChoice, computerSelection){
     computerChoiceDisplay.appendChild(compVal);
     compVal.textContent = computerSelection;
 
+    if (playercount === 5 || compcount === 5) {
+        playerVal.style.display = 'none';
+        compVal.style.display = 'none';
+    }
     if((playerChoice.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") || (playerChoice.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") || (playerChoice.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock"))
     {
+        scoreMessage.textContent = "\nYou lose " + computerSelection + " beats " + playerChoice;
+        scores.appendChild(scoreMessage);
         score = -1;
     }
     if((playerChoice.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") || (playerChoice.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") || (playerChoice.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"))
     {
+        scoreMessage.textContent = "\nYou win " + playerChoice + " beats " + computerSelection;
+        scores.appendChild(scoreMessage);
         score = 1;
     }
     if(playerChoice.toLowerCase() === computerSelection.toLowerCase()){
+        scoreMessage.textContent = "\nIts a Draw";
+        scores.appendChild(scoreMessage);
+        return;
+    }
+    if (!(playercount === 5 || compcount === 5)) {
+        updateScore(score);
+    }
+}
 
+function updateScore(value) {
+    
+    if(value === 1)
+    {
+        playercount++;
+        playerScore.textContent = playercount;
     }
+    else if(value === -1)
+    {
+        compcount++;
+        compScore.textContent = compcount;
+    }
+}
 
-    while (!(playercount || compcount) == 5) {
-        if(score == 1)
+rock.addEventListener('click', function() {playRound('rock', getComputerChoice()); checkGameStatus();});
+paper.addEventListener('click', function() {playRound('paper', getComputerChoice()); checkGameStatus();});
+scissors.addEventListener('click', function() {playRound('scissors', getComputerChoice()); checkGameStatus();});
+
+
+function checkGameStatus(){
+    if (playercount === 5 || compcount === 5) {
+        if(playercount > compcount)
         {
-            playercount++;
+            scoreMessage.textContent = "You Win! Refresh to play again";
+            scores.appendChild(scoreMessage);
         }
-        else if(score == -1)
+        else if(compcount > playercount)
         {
-            compcount++;
+            scoreMessage.textContent = "You Lose! Refresh to play again";
+            scores.appendChild(scoreMessage);
         }
-    }
-    if(playercount > compcount)
-    {
-        alert("You win " + playercount + " - " + compcount);
-    }
-    else if(compcount > playercount)
-    {
-        alert("You lose " + compcount + " - " + playercount);
     }
 }
 
